a2b9ba1d9864a7fcb2e7a3239e3ea3d353430b7d
4rkal <4rkal@proton.me>
2023-09-07 18:37:30 +0300

    check whether or not it is in the last week

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import feedparser
+from datetime import datetime, timedelta
+import pytz
 
 github_repo_links = [
     "https://github.com/monero-project/monero",
@@ -6,6 +8,12 @@ github_repo_links = [
     "https://github.com/4rkal/MoneroOS",
     "https://github.com/SChernykh/p2pool",
 ]
+
+now = datetime.utcnow()
+now = now.date()
+one_week_ago = now - timedelta(days=7)
+print(one_week_ago)
+print(now)
 
 # check for new updates in github repos
 def main():
@@ -16,9 +24,17 @@ def main():
         # print(rss_feed)
         if rss_feed.entries:
             latest_update = rss_feed.entries[0]
-            print(f"Latest update for {repo_link}:")
-            print(f"Title: {latest_update.get('title', 'N/A')}")
-            print(f"Link: {latest_update.get('link', 'N/A')}")
+            release_date = latest_update.get('updated')
+            release_date = datetime.fromisoformat(release_date).replace(tzinfo=pytz.UTC)
+            release_date = release_date.date()
+            # print(release_date)
+            if one_week_ago <= release_date <= now:
+                # print(latest_update)
+                print(f"Latest update for {repo_link}:")
+                print(f"Title: {latest_update.get('title', 'N/A')}")
+                print(f"Link: {latest_update.get('link', 'N/A')}")
+                print(f"Latest Update: {release_date} ")
+                print()
 
 if __name__ == "__main__":
     main()
